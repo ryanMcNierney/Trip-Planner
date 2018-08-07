ed4775d90f2cd20378a0ed31fb2d6f32b12a03de
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-08-07 18:10:12 -0400

    css updates

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import { Marker } from "mapbox-gl";
-const buildMarker = require("./marker")
-console.log("Hello from JavaScript")
+import { Marker } from 'mapbox-gl';
+const buildMarker = require('./marker');
 
+const mapboxgl = require('mapbox-gl');
 
-const mapboxgl = require("mapbox-gl");
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiamxlZTkwOTAiLCJhIjoiY2prazU3d29lMW16MDNwbzN2OTFrZzh2aiJ9.-L3Kfm74hWkXkorTApUvOw'
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiamxlZTkwOTAiLCJhIjoiY2prazU3d29lMW16MDNwbzN2OTFrZzh2aiJ9.-L3Kfm74hWkXkorTApUvOw';
 
 const map = new mapboxgl.Map({
-  container: "map",
+  container: 'map',
   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
   zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+  style: 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available.
 });
 
-const marker = buildMarker("house", [-74.009151, 40.705086]);
-marker.addTo(map)
+const marker = buildMarker('house', [-74.009151, 40.705086]);
+marker.addTo(map);
--- src/marker.js
+++ src/marker.js
@@ -1,28 +1,27 @@
-const mapboxgl = require("mapbox-gl");
+const mapboxgl = require('mapbox-gl');
 
 function buildMarker(type, coordinates) {
-  let url
-  switch(type) {
+  let url;
+  switch (type) {
     case 'activity':
-      url = "http://i.imgur.com/WbMOfMl.png";
+      url = 'http://i.imgur.com/WbMOfMl.png';
       break;
     case 'hotel':
-      url = "http://i.imgur.com/D9574Cu.png";
+      url = 'http://i.imgur.com/D9574Cu.png';
       break;
     case 'restaurant':
-      url = "http://i.imgur.com/cqR6pUI.png";
+      url = 'http://i.imgur.com/cqR6pUI.png';
       break;
     default:
-      url = "http://maps.google.com/mapfiles/ms/micons/yen.png";
+      url = 'http://maps.google.com/mapfiles/ms/micons/yen.png';
       break;
-    }
-    console.log("This is the url", url)
-    const markerDomEl = document.createElement("div");
-    markerDomEl.style.width = "32px";
-    markerDomEl.style.height = "39px";
-    markerDomEl.style.backgroundImage = `url(${url})`;
+  }
+  const markerDomEl = document.createElement('div');
+  markerDomEl.style.width = '32px';
+  markerDomEl.style.height = '39px';
+  markerDomEl.style.backgroundImage = `url(${url})`;
 
-    return new mapboxgl.Marker(markerDomEl).setLngLat(coordinates)
+  return new mapboxgl.Marker(markerDomEl).setLngLat(coordinates);
 }
 
 module.exports = buildMarker;
